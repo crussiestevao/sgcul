8df215ad4defa8f001ea2b1d7e3faa9010f72f38
Armando Paulo <51958179+armandopaulodev@users.noreply.github.com>
2024-05-20 11:49:30 +0200

    criation g userss

--- app/Http/Controllers/user/UserController.php
+++ app/Http/Controllers/user/UserController.php
@@ -3,18 +3,42 @@
 namespace App\Http\Controllers\user;
 
 use App\Http\Controllers\Controller;
+use App\Models\role\Role;
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 use Inertia\Inertia;
+use Illuminate\Support\Str;
 
 class UserController extends Controller
 {
-     public function index (){
-      
-      $users = User::all();
+     public function index()
+     {
 
-      $data = compact("users");
-      
-        return Inertia::render("User/User", $data);
+          $users = User::all();
+
+          $roles = Role::all();
+
+          $data = compact("users", 'roles');
+
+          return Inertia::render("User/User", $data);
+     }
+
+
+     public function store(Request $request)
+     {
+          $roles = Role::whereIn('id', $request->roles)->pluck('id');
+
+          $user = User::create([
+               'name' => $request->name,
+               'email' => $request->email,
+               'password' => $request->password ? Hash::make($request->password) : Hash::make(Str::lower(explode(' ', $request->name)[0]) . '@123')
+          ]);
+
+          $user->roles()->sync($roles);
+
+          $users = User::all();
+
+          return compact('users');
      }
 }
--- routes/user/user.php
+++ routes/user/user.php
@@ -8,7 +8,8 @@ use Illuminate\Support\Facades\Route;
 Route::controller(UserController::class)->group(function () {
 
     Route::middleware("auth")->group(function () {
-        Route::get("user/creation/view", [UserController::class, "index"])->name("user.create");
+        Route::get("user/creation/view", 'index')->name("user.create");
+        Route::post("user/creation/store", 'store')->name("user.store");
     });
 
 });
